f5a52e56d66bf251efd16e791adff68ce5b45703
ty-codes <titoadeoye00@gmail.com>
2024-01-06 15:52:52 -0800

    refactor: add header to page

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,33 @@ import "./index.scss";
 
 import Header from "home/Header";
 import Footer from "home/Footer";
-import SafeComponent from "./SafeComponent";
 import PDPContent from "./PDPContent";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = createRoot(document.getElementById("app"));
 
 const App = () => {
   return (
     <div className="text-2xl mx-auto max-w-6xl">
-      <SafeComponent>
-        <Header />
-      </SafeComponent>
+      <Header />
 
       <div className="my-10">
-        <Router>
+        <BrowserRouter>
           <Routes>
             <Route path="products/:id" element={<PDPContent />} />
           </Routes>
-        </Router>
+        </BrowserRouter>
       </div>
       <Footer />
     </div>
   );
 };
-
+{
+  /* <Router>
+          <Routes>
+            <Route path="products/:id" element={<PDPContent />} />
+          </Routes>
+        </Router>  */
+}
 root.render(<App />);
